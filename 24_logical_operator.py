577cd39885345193886aca18a309944b1da7ea2e
tahimulislamtamim4-cmd <tahimulislamtamim4@gmail.com>
2026-07-19 13:46:11 -0600

    Create 24_logical_operator.py

---
+++ 24_logical_operator.py
@@ -0,0 +1,6 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("AND Operator =", a > 0 and b > 0)
+print("OR Operator =", a > 0 or b > 0)
+print("NOT Operator =", not(a > 0))
