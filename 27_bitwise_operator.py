fcc8d61644e74c81e7d0065f13b2d8a0355e99df
tahimulislamtamim4-cmd <tahimulislamtamim4@gmail.com>
2026-07-19 13:51:59 -0600

    Create 27_bitwise_operator.py

---
+++ 27_bitwise_operator.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Bitwise AND =", a & b)
+print("Bitwise OR =", a | b)
+print("Bitwise XOR =", a ^ b)
+print("Bitwise NOT of a =", ~a)
+print("Left Shift =", a << 1)
+print("Right Shift =", a >> 1)
