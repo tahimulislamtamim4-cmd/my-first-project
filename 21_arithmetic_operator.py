d42f337ad62ae79c61b71089ac26990de9c11a61
tahimulislamtamim4-cmd <tahimulislamtamim4@gmail.com>
2026-07-19 13:37:38 -0600

    Create 21_arithmetic_operator.py

---
+++ 21_arithmetic_operator.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+print("Remainder =", a % b)
+print("Floor Division =", a // b)
+print("Power =", a ** b)
