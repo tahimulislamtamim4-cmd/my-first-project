cf2dcf62ae64437dff506851704d49fb9d27575b
tahimulislamtamim4-cmd <tahimulislamtamim4@gmail.com>
2026-07-21 04:50:58 -0600

    Create 81_perfect_number.py

---
+++ 81_perfect_number.py
@@ -0,0 +1,12 @@
+n = int(input("Enter a number: "))
+
+sum = 0
+
+for i in range(1, n):
+    if n % i == 0:
+        sum = sum + i
+
+if sum == n:
+    print("Perfect Number")
+else:
+    print("Not Perfect Number")
