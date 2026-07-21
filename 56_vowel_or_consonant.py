f124ad1dd4e0cf47cd7ea2b1b14317c599458916
tahimulislamtamim4-cmd <tahimulislamtamim4@gmail.com>
2026-07-20 21:51:18 -0600

    Create 56_vowel_or_consonant.py

---
+++ 56_vowel_or_consonant.py
@@ -0,0 +1,6 @@
+ch = input("Enter a character: ")
+
+if ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A' or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U':
+    print("Vowel")
+else:
+    print("Consonant")
