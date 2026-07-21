315fa02e854a84825a1e8a55c9da2c68299fc9f5
tahimulislamtamim4-cmd <tahimulislamtamim4@gmail.com>
2026-07-20 21:37:57 -0600

    Create 51_grade_calculator.py

---
+++ 51_grade_calculator.py
@@ -0,0 +1,16 @@
+marks = int(input("Enter marks: "))
+
+if marks >= 80:
+    print("Grade = A+")
+elif marks >= 70:
+    print("Grade = A")
+elif marks >= 60:
+    print("Grade = A-")
+elif marks >= 50:
+    print("Grade = B")
+elif marks >= 40:
+    print("Grade = C")
+elif marks >= 33:
+    print("Grade = D")
+else:
+    print("Grade = F")
