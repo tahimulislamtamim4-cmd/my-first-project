4f07ac1a2b4a8770a8a1dad30ee8a1650e188f34
tahimulislamtamim4-cmd <tahimulislamtamim4@gmail.com>
2026-07-21 20:31:02 -0600

    Create 99_right_triangle_pattern.py

---
+++ 99_right_triangle_pattern.py
@@ -0,0 +1,8 @@
+rows = int(input("Enter rows: "))
+
+for i in range(1, rows + 1):
+    for j in range(rows - i):
+        print(" ", end="")
+    for j in range(i):
+        print("*", end=" ")
+    print()
