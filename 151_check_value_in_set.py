86e826fdb783af40a27fea161bb6ebbfb85d6831
tahimulislamtamim4-cmd <tahimulislamtamim4@gmail.com>
2026-07-24 10:22:54 -0600

    Create 151_check_value_in_set.py

---
+++ 151_check_value_in_set.py
@@ -0,0 +1,8 @@
+my_set = {10, 20, 30, 40, 50}
+
+value = int(input("Enter a value: "))
+
+if value in my_set:
+    print("Value exists in the set")
+else:
+    print("Value does not exist in the set")
