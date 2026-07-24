93f834cc02127ff94a2109a338ba18c747127fbf
tahimulislamtamim4-cmd <tahimulislamtamim4@gmail.com>
2026-07-24 05:27:01 -0600

    Create 118_replace.py

---
+++ 118_replace.py
@@ -0,0 +1,7 @@
+text = input("Enter a string: ")
+old = input("Enter the word to replace: ")
+new = input("Enter the new word: ")
+
+result = text.replace(old, new)
+
+print("Updated String =", result)
